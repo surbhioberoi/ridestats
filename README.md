796a2f3684a16316d6d1a6e0907a752593f92de7
Surbhi Oberoi <siyaoberoi93@gmail.com>
2015-12-16 17:14:01 +0530

    Adding more stats about total distance, time spent waiting, time spent travelling etc. More features to be added.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # ridestats
 An app to view cool stats about your Uber rides
+
+* Name and promo code.
+* Number of cities where Uber was used.
+* Number of rides taken and in different product types.
+* Kilometers travelled in Uber.
+* Total time spent waiting for an Uber.
+* Total time spent in an Uber.
+* Map displaying cities travelled along with the count of rides taken in each.
+
   
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 from uber_rides.auth import AuthorizationCodeGrant
 from uber_rides.client import UberRidesClient
 import json
+from collections import defaultdict
 
 CLIENT_ID = "TmORsdHDdk3GgGx3bBCE_Jh5EzaDbPKc"
 CLIENT_SECRET = "dsPSgtSom4xyqgu8QcP-RPZ_DDju-pP-OKQdtMtW"
@@ -34,6 +35,11 @@ def post_login():
 	ride_count = result['count']
 	complete_history.extend(result['history'])
 
+	client_data = client.get_user_profile().json
+	first_name = client_data['first_name']
+	last_name = client_data['last_name']
+	promo_code = client_data['promo_code']
+
 	times_ran = 1
 
 	for i in range (ride_count/50) :
@@ -43,12 +49,48 @@ def post_login():
 		times_ran += 1
 
 	cities = set()
+	product_types = defaultdict(int)
+	total_distance = 0
+	wait_time = 0
+	trip_time = 0
+
 	for i in complete_history:
 		cities.add(i['start_city']['display_name'])
+		product_types[i['product_id']] += 1
+		total_distance += i['distance']
+		wait_time += (i['start_time'] - i['request_time'])/(60.0*60.0)
+		trip_time += (i['end_time'] - i['start_time'])/(60.0*60.0)
+
+	products = {}
+
+	for key in product_types:
+		try:
+			productinfo = client.get_product(key)
+			products[key] = productinfo.json
+		except:
+			pass
+
+	uber_products =  defaultdict(int)
+	for key in product_types:
+		try:
+			uber_products[products[key]['display_name']] += product_types[key]
+		except:
+		 	pass
+
+	print uber_products
+
+	float_format = lambda x: float("{0:.2f}".format(x))
 
 	return render_template('results.html',
 						   ride_count=ride_count,
-						   cities_count=len(cities))
+						   cities_count=len(cities),
+						   firstname=first_name,
+						   lastname=last_name,
+						   promocode=promo_code,
+						   products=uber_products,
+						   total_distance=float_format(total_distance*1.60934),
+						   waittime=float_format(wait_time),
+						   trip_time=float_format(trip_time))
 
 
 
